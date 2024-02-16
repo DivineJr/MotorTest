191bc5f8dead2831ee5e3847a61c749dc81a6334
DivineJr <divineholinessjr@gmail.com>
2024-02-15 21:06:43 -0500

    Planning for finished product (oh no)

---
+++ src/main/java/frc/robot/ShuffleboardStuff/ShuffleList.java
@@ -0,0 +1,11 @@
+package frc.robot.ShuffleboardStuff;
+
+public class ShuffleList {
+    /**
+     * A linked list that takes in:
+     *      - Title of widget
+     *      - Data type of widget
+     *      - widget object type (Simple, Complex, etc.)
+     */
+    
+}
---
+++ src/main/java/frc/robot/ShuffleboardStuff/ShuffleboardHandler.java
@@ -0,0 +1,22 @@
+package frc.robot.ShuffleboardStuff;
+
+public class ShuffleboardHandler {
+    /*
+     * TODO:
+     * Take in a list of planned components
+     *      - Linked List of planned components
+     *          - Title of Component
+     *          - Data type of Component
+     *          - Widget Type (object type it will show up as)
+     * Check for existance of component on initialization
+     *      - If it doesn't exist, create it!
+     *      - Make a new list with all the GenericEntry's of the widgets
+     * Then, use this class in order to interact with Shuffleboard
+     *      - Search the list for an object of a specified title and return the GenericEntry with that title
+     *      - Return null if object not found
+     * 
+     * What's the point of this class? To eliminate errors caused by existing widgets in the Shuffleboard on creation
+     * 
+     */
+    
+}
